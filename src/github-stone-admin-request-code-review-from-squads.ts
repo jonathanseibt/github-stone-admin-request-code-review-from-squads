0ccc08d5e43ca60e75a6c4741135f30350c22c74
jonathanseibt <hey@jonathanseibt.com>
2023-06-12 21:34:28 -0300

    feat: improve team members list

--- src/github-stone-admin-request-code-review-from-squads.ts
+++ src/github-stone-admin-request-code-review-from-squads.ts
@@ -25,28 +25,28 @@
     whenHide('#reviewers-select-menu .select-menu-list svg.anim-rotate', (): void => {
       whenShow('#reviewers-select-menu > details-menu .select-menu-list', (element): void => {
         if (!get('#squad-platform-ux-button')) {
-          element.innerHTML = buildReviewersButtonHtml('squad-platform-ux-button', 'Squad Platform & UX', 'Lucas, Paulinho, e Vini') + element.innerHTML;
+          element.innerHTML = buildReviewersButtonHtml('squad-platform-ux-button', 'Squad Platform & UX', 'Lucas, Paulinho, Vini') + element.innerHTML;
           event('click', '#squad-platform-ux-button', (): void => {
             requestCodeReviewFrom([REVIEWERS.Lucas, REVIEWERS.Paulinho, REVIEWERS.Vini]);
           });
         }
 
         if (!get('#squad-banking-capital-button')) {
-          element.innerHTML = buildReviewersButtonHtml('squad-banking-capital-button', 'Squad Banking & Capital', 'Hoffmann, Malê, Phelipe, Thiago, e Vini') + element.innerHTML;
+          element.innerHTML = buildReviewersButtonHtml('squad-banking-capital-button', 'Squad Banking & Capital', 'Hoffmann, Malê, Phelipe, Thiago, Vini') + element.innerHTML;
           event('click', '#squad-banking-capital-button', (): void => {
             requestCodeReviewFrom([REVIEWERS.Hoffmann, REVIEWERS.Male, REVIEWERS.Phelipe, REVIEWERS.Thiago, REVIEWERS.Vini]);
           });
         }
 
         if (!get('#squad-risk-lifecycle-button')) {
-          element.innerHTML = buildReviewersButtonHtml('squad-risk-lifecycle-button', 'Squad Risk & Lifecycle', 'Ermerson, Guh, e Matheus') + element.innerHTML;
+          element.innerHTML = buildReviewersButtonHtml('squad-risk-lifecycle-button', 'Squad Risk & Lifecycle', 'Ermerson, Guh, Matheus') + element.innerHTML;
           event('click', '#squad-risk-lifecycle-button', (): void => {
             requestCodeReviewFrom([REVIEWERS.Ermerson, REVIEWERS.Guh, REVIEWERS.Matheus]);
           });
         }
 
         if (!get('#squad-payments-lifecycle-button')) {
-          element.innerHTML = buildReviewersButtonHtml('squad-payments-lifecycle-button', 'Squad Payments & Lifecycle', 'Amandinha, Ka, Pitta, Seibt, e Sidney') + element.innerHTML;
+          element.innerHTML = buildReviewersButtonHtml('squad-payments-lifecycle-button', 'Squad Payments & Lifecycle', 'Amandinha, Ka, Pitta, Seibt, Sidney') + element.innerHTML;
           event('click', '#squad-payments-lifecycle-button', (): void => {
             requestCodeReviewFrom([REVIEWERS.Amandinha, REVIEWERS.Ka, REVIEWERS.Pitta, REVIEWERS.Seibt, REVIEWERS.Sidney]);
           });
